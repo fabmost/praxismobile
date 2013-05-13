a1a45934853d775b8b780068dde06f1c2c25765b
ROHF <rohf@praxis.com.mx>
2013-05-13 13:27:58 -0500

    ImageSwitcher en Inicio y imageView casero

--- PraxisMobile/src/com/praxismobile/vistas/FragmentoDemo.java
+++ PraxisMobile/src/com/praxismobile/vistas/FragmentoDemo.java
@@ -5,19 +5,50 @@ import com.praxismobile.R;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class FragmentoDemo extends Fragment{
+	
+	private Integer[] imgIds = {
+		R.drawable.slideshow_1,
+		R.drawable.slideshow_2,
+		R.drawable.slideshow_3,
+		R.drawable.slideshow_4,
+		R.drawable.slideshow_5,
+		R.drawable.slideshow_6,
+	};
+	
+	private int imgCount = 1;
+	
+	Handler m_Handler = new Handler();
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		View view = inflater.inflate(R.layout.fragmenttest, container,false);
+		
 		Button btn = (Button)view.findViewById(R.id.button1);
+		final ImageView imgSw = (ImageView)view.findViewById(R.id.imgswitcher);
+		
+		Runnable swapImage = new Runnable() {
+		    @Override
+		    public void run() {
+		    	imgSw.setImageResource(imgIds[imgCount]);
+		    	imgCount++;
+		    	if(imgCount > (imgIds.length-1)){
+		    		imgCount = 0;
+				}
+		    	m_Handler.postDelayed(this, (5 * 1000));
+		    }
+		};
+		
+		m_Handler.post(swapImage);
 		btn.setOnClickListener(new OnClickListener() {
 			
 			@Override
@@ -30,7 +61,7 @@ public class FragmentoDemo extends Fragment{
 	}
 	
 	public void boton(){
-		Intent lVideoIntent = new Intent(null, Uri.parse("ytv://"+"vu85Ygz5XS8"), getActivity(), IntroVideoActivity.class);
+		Intent lVideoIntent = new Intent(null, Uri.parse("ytv://"+"R5nAASDkeOk"), getActivity(), IntroVideoActivity.class);
 	    startActivity(lVideoIntent);
 	}
 
